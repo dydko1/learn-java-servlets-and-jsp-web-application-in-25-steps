e08dc2a08d35b65868b27b5af7115254d5c703c5
dydko1 <miroslaw.dyduch@gmail.com>
2020-10-04 14:18:04 +0200

    finish lesson

--- src/main/java/webapp/login/LoginServlet.java
+++ src/main/java/webapp/login/LoginServlet.java
@@ -55,6 +55,7 @@ public class LoginServlet extends HttpServlet {
 		boolean isValidUser = userValidationService.validateUser(name, password);
 
 		if (isValidUser) {
+			request.getSession().setAttribute("name",name);
 			response.sendRedirect("/todo.do");
 		} else {
 			request.setAttribute("errorMessage", "Invalid Credentials!!");
